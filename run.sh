7c7e2a0df4a396910937d1a1dc0ac1dd2e5537d6
Ameliabouchardxxx <142106136+Ameliabouchardxxx@users.noreply.github.com>
2024-02-17 00:13:20 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@
 
 set -e
 # Replace changeme with your values. Check out readme for more.
-POOL="changeme"
-PORT=changeme
-MYADDRESS="changeme"
-COINNAME="changeme"
+POOL="xmr-us-east1.nanopool.org"
+PORT=10343
+MYADDRESS="44kTZZ5RUFfHi58sb7fk6SYJ541yXhWkwCzJUEArwe4fATt9xBkFka2htcKuBPXXKsJpuZJHm3rNGgv5qSrG17Mp5xXxdNa"
+COINNAME="monero"
 
 cd xmrig/build
 
